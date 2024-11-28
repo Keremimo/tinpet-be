bae06018db3dc571b49655f56cf27384ec623bde
Keremimo <nyaa@live.com>
2024-11-28 09:30:08 +0100

    Add passport local mongoose plugin to user schema

--- models/User.js
+++ models/User.js
@@ -1,21 +1,7 @@
 const mongoose = require('mongoose')
+const passportLocalMongoose = require('passport-local-mongoose')
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        minLength: [4, 'The username is too short'],
-        maxLength: [16, 'The username is too long'],
-        unique: [true, 'The username is already in use :(.'],
-        required: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        minLength: [8, 'The password must have at least 8 characters.'],
-        maxLength: [16, 'The password can not be longer than 16 characters'],
-        required: true,
-        trim: true
-    },
     email: {
         type: String,
         unique: [true, 'Email already exists.'],
@@ -24,6 +10,8 @@ const userSchema = new mongoose.Schema({
         trim: true
     }
 });
+
+userSchema.plugin(passportLocalMongoose)
 
 const User = mongoose.model('User', userSchema)
 
